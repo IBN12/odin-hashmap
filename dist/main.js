88e29e98807013280ecf3a19bfb1dae739bf487b
IBN12 <IBN12@github.com>
2024-01-24 23:36:36 -0800

    Webpack and Jest testing environments are set up in the appication.

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Project HashMap'); // Testing
+console.log('Webpack is being used for this project...'); // Testing 
+console.log('Test Driven Development (TDD) is all set up for testing and mocking...'); // Testing 
+console.log('\n'); // Testing
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGdDQUFnQztBQUNoQywwREFBMEQ7QUFDMUQsdUZBQXVGO0FBQ3ZGLG1CQUFtQiIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4taGFzaG1hcC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnUHJvamVjdCBIYXNoTWFwJyk7IC8vIFRlc3RpbmdcbmNvbnNvbGUubG9nKCdXZWJwYWNrIGlzIGJlaW5nIHVzZWQgZm9yIHRoaXMgcHJvamVjdC4uLicpOyAvLyBUZXN0aW5nIFxuY29uc29sZS5sb2coJ1Rlc3QgRHJpdmVuIERldmVsb3BtZW50IChUREQpIGlzIGFsbCBzZXQgdXAgZm9yIHRlc3RpbmcgYW5kIG1vY2tpbmcuLi4nKTsgLy8gVGVzdGluZyBcbmNvbnNvbGUubG9nKCdcXG4nKTsgLy8gVGVzdGluZ1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,4 @@
+console.log('Project HashMap'); // Testing
+console.log('Webpack is being used for this project...'); // Testing 
+console.log('Test Driven Development (TDD) is all set up for testing and mocking...'); // Testing 
+console.log('\n'); // Testing
